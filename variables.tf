7734aa3352a8daf55bd71da2f92c4946d524131b
Dincho Todorov <dincho@users.noreply.github.com>
2019-05-27 12:38:05 +0300

    Bump testnet default package version to 3.0.1

--- variables.tf
+++ variables.tf
@@ -1,3 +1,11 @@
 variable "vault_addr" {
   description = "Vault server URL address"
 }
+
+variable "bootstrap_version" {
+  default = "v2.2.0"
+}
+
+variable "package" {
+  default = "https://releases.ops.aeternity.com/aeternity-3.0.1-ubuntu-x86_64.tar.gz"
+}
