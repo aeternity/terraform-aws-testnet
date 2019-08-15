11c5ea62a9bd20c1d697381044f018a387d1d7d6
Dincho Todorov <dincho@users.noreply.github.com>
2019-08-15 17:06:22 +0300

    Bump default package version to 4.2.0

--- variables.tf
+++ variables.tf
@@ -7,5 +7,5 @@ variable "bootstrap_version" {
 }
 
 variable "package" {
-  default = "https://releases.ops.aeternity.com/aeternity-4.1.0-ubuntu-x86_64.tar.gz"
+  default = "https://releases.ops.aeternity.com/aeternity-4.2.0-ubuntu-x86_64.tar.gz"
 }
