c054f8772f48e2889b84000bc1b9a4252679b6bd
Ivaylo Badinov <632282+venimus@users.noreply.github.com>
2019-11-05 11:25:22 +0200

    Use vault-config for bootstrap. Update to 5.0.2 (#14)

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "aws_deploy-ap-southeast-1" {
-  source            = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v2.1.0"
+  source            = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v2.3.1"
   env               = "uat"
   color             = "blue"
   bootstrap_version = var.bootstrap_version
@@ -28,7 +28,7 @@ module "aws_deploy-ap-southeast-1" {
 }
 
 module "aws_deploy-eu-central-1" {
-  source            = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v2.1.0"
+  source            = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v2.3.1"
   env               = "uat"
   color             = "blue"
   bootstrap_version = var.bootstrap_version
@@ -57,7 +57,7 @@ module "aws_deploy-eu-central-1" {
 }
 
 module "aws_deploy-us-west-2" {
-  source            = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v2.1.0"
+  source            = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v2.3.1"
   env               = "uat"
   color             = "green"
   bootstrap_version = var.bootstrap_version
@@ -86,7 +86,7 @@ module "aws_deploy-us-west-2" {
 }
 
 module "aws_deploy-uat-eu-north-1" {
-  source            = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v2.1.0"
+  source            = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v2.3.1"
   env               = "uat"
   color             = "green"
   bootstrap_version = var.bootstrap_version
@@ -115,12 +115,13 @@ module "aws_deploy-uat-eu-north-1" {
 }
 
 module "aws_deploy-uat_mon-ap-southeast-1" {
-  source            = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v2.1.0"
+  source            = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v2.3.1"
   env               = "uat_mon"
   color             = "blue"
   bootstrap_version = var.bootstrap_version
   vault_role        = "ae-node"
   vault_addr        = var.vault_addr
+  node_config       = "secret/aenode/config/uat_mon@ap-southeast-1"
 
   spot_nodes_min = 1
   spot_nodes_max = 1
@@ -143,12 +144,13 @@ module "aws_deploy-uat_mon-ap-southeast-1" {
 }
 
 module "aws_deploy-uat_mon-eu-central-1" {
-  source            = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v2.1.0"
+  source            = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v2.3.1"
   env               = "uat_mon"
   color             = "blue"
   bootstrap_version = var.bootstrap_version
   vault_role        = "ae-node"
   vault_addr        = var.vault_addr
+  node_config       = "secret/aenode/config/uat_mon@eu-central-1"
 
   spot_nodes_min = 1
   spot_nodes_max = 1
@@ -171,12 +173,13 @@ module "aws_deploy-uat_mon-eu-central-1" {
 }
 
 module "aws_deploy-uat_mon-us-west-2" {
-  source            = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v2.1.0"
+  source            = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v2.3.1"
   env               = "uat_mon"
   color             = "green"
   bootstrap_version = var.bootstrap_version
   vault_role        = "ae-node"
   vault_addr        = var.vault_addr
+  node_config       = "secret/aenode/config/uat_mon@us-west-2"
 
   spot_nodes_min = 1
   spot_nodes_max = 1
@@ -199,12 +202,13 @@ module "aws_deploy-uat_mon-us-west-2" {
 }
 
 module "aws_deploy-uat_mon-eu-north-1" {
-  source            = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v2.1.0"
+  source            = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v2.3.1"
   env               = "uat_mon"
   color             = "green"
   bootstrap_version = var.bootstrap_version
   vault_role        = "ae-node"
   vault_addr        = var.vault_addr
+  node_config       = "secret/aenode/config/uat_mon@eu-north-1"
 
   spot_nodes_min = 1
   spot_nodes_max = 1
