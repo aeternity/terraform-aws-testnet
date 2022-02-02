2122bd9d72c77443be151cce9208a6d13b415479
Michał <github@bagrowski.com>
2022-02-02 21:26:21 +0100

    bmp terraform to 1.1.4 (#44)
    * bmp terraform to 1.1.4

---
+++ terraform.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = "1.1.4"
+  required_providers {
+    aws = {
+      version = "2.70.0"
+    }
+    random = {
+      version = "3.1.0"
+    }
+    template = {
+      version = "2.2.0"
+    }
+  }
+}
