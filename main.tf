671f83e68a115975baef81bca07bd5dcea0b2c02
Dincho Todorov <dincho@users.noreply.github.com>
2025-04-09 12:52:02 +0300

    Remove backup and spot nodes

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ module "aws_deploy-ap-southeast-1" {
   env    = "uat"
 
   static_nodes   = 1
-  spot_nodes_min = 2
-  spot_nodes_max = 2
+  spot_nodes_min = 0
+  spot_nodes_max = 0
 
   instance_type  = "m5.large"
   instance_types = ["m6i.large", "m5.large"]
@@ -37,8 +37,8 @@ module "aws_deploy-us-west-2" {
   env    = "uat"
 
   static_nodes   = 1
-  spot_nodes_min = 2
-  spot_nodes_max = 2
+  spot_nodes_min = 0
+  spot_nodes_max = 0
 
   instance_type  = "m5.large"
   instance_types = ["m6i.large", "m5.large"]
@@ -71,8 +71,8 @@ module "aws_deploy-uat-eu-north-1" {
   env    = "uat"
 
   static_nodes   = 1
-  spot_nodes_min = 1
-  spot_nodes_max = 1
+  spot_nodes_min = 0
+  spot_nodes_max = 0
 
   instance_type  = "m5.large"
   instance_types = ["m6i.large", "m5.large"]
@@ -133,66 +133,66 @@ module "aws_deploy-uat_mon" {
   }
 }
 
-module "aws_deploy-uat_backup_full" {
-  source = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v4.0.0"
-  env    = "uat_backup"
+# module "aws_deploy-uat_backup_full" {
+#   source = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v4.0.0"
+#   env    = "uat_backup"
 
-  static_nodes = 1
+#   static_nodes = 1
 
-  instance_type  = "t3.medium"
-  instance_types = ["t3.medium", "t3.large", "c6i.large"]
-  ami_name       = "aeternity-ubuntu-22.04-v1709639419"
+#   instance_type  = "t3.medium"
+#   instance_types = ["t3.medium", "t3.large", "c6i.large"]
+#   ami_name       = "aeternity-ubuntu-22.04-v1709639419"
 
-  root_volume_size        = 20
-  additional_storage      = true
-  additional_storage_size = 300
+#   root_volume_size        = 20
+#   additional_storage      = true
+#   additional_storage_size = 300
 
-  tags = {
-    role = "aenode"
-    env  = "uat_backup"
-    kind = "full"
-  }
+#   tags = {
+#     role = "aenode"
+#     env  = "uat_backup"
+#     kind = "full"
+#   }
 
-  config_tags = {
-    vault_role        = "ae-node"
-    vault_addr        = var.vault_addr
-    bootstrap_version = var.bootstrap_version
-    bootstrap_config  = "secret2/aenode/config/uat_backup_full"
-  }
+#   config_tags = {
+#     vault_role        = "ae-node"
+#     vault_addr        = var.vault_addr
+#     bootstrap_version = var.bootstrap_version
+#     bootstrap_config  = "secret2/aenode/config/uat_backup_full"
+#   }
 
-  providers = {
-    aws = aws.eu-north-1
-  }
-}
+#   providers = {
+#     aws = aws.eu-north-1
+#   }
+# }
 
-module "aws_deploy-uat_backup_light" {
-  source = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v4.0.0"
-  env    = "uat_backup"
+# module "aws_deploy-uat_backup_light" {
+#   source = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v4.0.0"
+#   env    = "uat_backup"
 
-  static_nodes = 1
+#   static_nodes = 1
 
-  instance_type  = "t3.medium"
-  instance_types = ["t3.medium", "t3.large", "c6i.large"]
-  ami_name       = "aeternity-ubuntu-22.04-v1709639419"
+#   instance_type  = "t3.medium"
+#   instance_types = ["t3.medium", "t3.large", "c6i.large"]
+#   ami_name       = "aeternity-ubuntu-22.04-v1709639419"
 
-  root_volume_size        = 20
-  additional_storage      = true
-  additional_storage_size = 300
+#   root_volume_size        = 20
+#   additional_storage      = true
+#   additional_storage_size = 300
 
-  tags = {
-    role = "aenode"
-    env  = "uat_backup"
-    kind = "light"
-  }
+#   tags = {
+#     role = "aenode"
+#     env  = "uat_backup"
+#     kind = "light"
+#   }
 
-  config_tags = {
-    vault_role        = "ae-node"
-    vault_addr        = var.vault_addr
-    bootstrap_version = var.bootstrap_version
-    bootstrap_config  = "secret2/aenode/config/uat_backup_light"
-  }
+#   config_tags = {
+#     vault_role        = "ae-node"
+#     vault_addr        = var.vault_addr
+#     bootstrap_version = var.bootstrap_version
+#     bootstrap_config  = "secret2/aenode/config/uat_backup_light"
+#   }
 
-  providers = {
-    aws = aws.eu-north-1
-  }
-}
+#   providers = {
+#     aws = aws.eu-north-1
+#   }
+# }
